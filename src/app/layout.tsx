fa9f0a706f0854a0d3ee9072d7cabe417960d848
Hisakiyo <34187647+hisakiyo@users.noreply.github.com>
2025-04-01 18:31:30 +0200

    add tailwind + mui + default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,49 @@
+import '@/app/styles/globals.css';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@/app/styles/theme';
+import AppNavbar from '@/app/components/layout/AppNavbar';
+import Header from '@/app/components/layout/Header';
+import SideMenu from '@/app/components/layout/SideMenu';
+import { Box, Stack } from '@mui/material';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className="bg-slate-100">{children}</body>
+    <html lang="en" className={roboto.variable}>
+      <body className="bg-slate-100">
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+          <Box sx={{ display: 'flex' }}>
+            <SideMenu />
+            <AppNavbar />
+            {/* Main content */}
+            <Box
+              component="main">
+              <Stack
+                spacing={2}
+                sx={{
+                  alignItems: 'center',
+                  mx: 3,
+                  pb: 5,
+                  mt: { xs: 8, md: 0 },
+                }}
+              >
+                <Header />
+                {children}
+              </Stack>
+            </Box>
+          </Box>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
-}
+};
